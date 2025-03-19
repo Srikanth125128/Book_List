3c507cd60480ae9ca5409f28fc63430bf0fdba30
Srikanth125128 <srikanthmetpally70@gmail.com>
2025-03-19 17:05:08 +0530

    change default author name to Srikanth in models.py

--- Book/models.py
+++ Book/models.py
@@ -2,6 +2,6 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    publisher = models.CharField(max_length =255,default="unknown publisher")
+    publisher = models.CharField(max_length =255,default="Srikanth")
     published_year = models.IntegerField(default=2020)
 
