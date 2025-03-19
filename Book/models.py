37176995c91db0d7546f7c57a8967b638eb4a7c2
Srikanth125128 <srikanthmetpally70@gmail.com>
2025-03-19 16:35:40 +0530

    change default year of books in models.py

--- Book/models.py
+++ Book/models.py
@@ -3,5 +3,6 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     publisher = models.CharField(max_length =255,default="unknown publisher")
-    published_year = models.IntegerField(default=2020)
+    published_year = models.IntegerField(default=2019)
+
 
